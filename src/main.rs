b33c62f6f14cf2f2f0e4fb36cc84802fefdbcbcc
Anant Narayan <anantnrg@gmail.com>
2025-03-18 15:11:53 +0530

    use new RTIC

--- src/main.rs
+++ src/main.rs
@@ -3,96 +3,71 @@
 #![no_main]
 #![no_std]
 
-use mpu6050::Mpu6050;
 use panic_rtt_target as _;
 use rtic::app;
+use rtic_monotonics::systick::prelude::*;
 use rtt_target::{rprintln, rtt_init_print};
-use stm32f1xx_hal::{
-    i2c::{BlockingI2c, Mode},
-    pac,
-    prelude::*,
-};
-use systick_monotonic::{fugit::Duration, Systick};
+use stm32f1xx_hal::gpio::{Output, PinState, PushPull, PC13};
+use stm32f1xx_hal::prelude::*;
+
+systick_monotonic!(Mono, 1000);
 
 #[app(device = stm32f1xx_hal::pac, peripherals = true, dispatchers = [SPI1])]
 mod app {
     use super::*;
 
     #[shared]
-    struct Shared {
-        mpu: Mpu6050<BlockingI2c<pac::I2C1>>,
-    }
+    struct Shared {}
 
     #[local]
-    struct Local {}
-
-    #[monotonic(binds = SysTick, default = true)]
-    type MonoTimer = Systick<1000>;
+    struct Local {
+        led: PC13<Output<PushPull>>,
+        state: bool,
+    }
 
     #[init]
-    fn init(cx: init::Context) -> (Shared, Local, init::Monotonics) {
+    fn init(cx: init::Context) -> (Shared, Local) {
+        // Setup clocks
         let mut flash = cx.device.FLASH.constrain();
-        let mut rcc = cx.device.RCC.constrain();
+        let rcc = cx.device.RCC.constrain();
 
-        let mono = Systick::new(cx.core.SYST, 36_000_000);
+        // Initialize the systick interrupt & obtain the token to prove that we did
+        Mono::start(cx.core.SYST, 36_000_000);
 
         rtt_init_print!();
-        rprintln!("Initializing MPU6050...");
+        rprintln!("init");
 
-        let clocks = rcc
+        let _clocks = rcc
             .cfgr
             .use_hse(8.MHz())
             .sysclk(36.MHz())
             .pclk1(36.MHz())
             .freeze(&mut flash.acr);
 
-        // Setup I2C (PB6 = SCL, PB7 = SDA)
-        let mut afio = cx.device.AFIO.constrain();
-        let mut gpioa = cx.device.GPIOA.split();
-        let mut gpiob = cx.device.GPIOB.split();
+        // Setup LED
+        let mut gpioc = cx.device.GPIOC.split();
+        let led = gpioc
+            .pc13
+            .into_push_pull_output_with_state(&mut gpioc.crh, PinState::Low);
 
-        let scl = gpiob.pb6.into_alternate_open_drain(&mut gpiob.crl);
-        let sda = gpiob.pb7.into_alternate_open_drain(&mut gpiob.crl);
+        // Schedule the blinking task
+        blink::spawn().ok();
 
-        let i2c = BlockingI2c::i2c1(
-            cx.device.I2C1,
-            (scl, sda),
-            &mut afio.mapr,
-            Mode::Fast {
-                frequency: 400.kHz(),
-                duty_cycle: stm32f1xx_hal::i2c::DutyCycle::Ratio2to1,
-            },
-            clocks,
-            1000,
-            10,
-            1000,
-            1000,
-        );
-
-        let mut mpu = Mpu6050::new(i2c);
-        mpu.init().unwrap();
-
-        // Schedule the sensor read task
-        read_mpu::spawn_after(Duration::<u64, 1, 1000>::from_ticks(1000)).unwrap();
-
-        (Shared { mpu }, Local {}, init::Monotonics(mono))
+        (Shared {}, Local { led, state: false })
     }
 
-    #[task(shared = [mpu])]
-    fn read_mpu(mut cx: read_mpu::Context) {
-        let accel = cx.shared.mpu.lock(|mpu| mpu.get_acc_angles().unwrap());
-        let gyro = cx.shared.mpu.lock(|mpu| mpu.get_gyro().unwrap());
-
-        rprintln!(
-            "Accel: x={} y={} z={}, Gyro: x={} y={} z={}",
-            accel.x,
-            accel.y,
-            accel.z,
-            gyro.x,
-            gyro.y,
-            gyro.z
-        );
-
-        read_mpu::spawn_after(Duration::<u64, 1, 1000>::from_ticks(1000)).unwrap();
+    #[task(local = [led, state])]
+    async fn blink(cx: blink::Context) {
+        loop {
+            rprintln!("blink");
+            if *cx.local.state {
+                cx.local.led.set_high();
+                *cx.local.state = false;
+            } else {
+                cx.local.led.set_low();
+                *cx.local.state = true;
+            }
+            Mono::delay(1000.millis()).await;
+        }
     }
 }
