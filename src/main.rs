dcd59546b5c45422929c680e698d3980a76c96f7
Anant Narayan <anantnrg@gmail.com>
2025-03-18 21:31:34 +0530

    why not work!!!

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,84 @@
-#![deny(unsafe_code)]
-#![deny(warnings)]
-#![no_main]
 #![no_std]
+#![no_main]
 
-use panic_rtt_target as _;
-use rtic::app;
-use rtic_monotonics::systick::prelude::*;
+use cortex_m::delay::Delay;
+use cortex_m_rt::entry;
+use embedded_hal::i2c::I2c;
+use mpu6050::Mpu6050;
+use panic_halt as _;
 use rtt_target::{rprintln, rtt_init_print};
-use stm32f1xx_hal::gpio::{Output, PinState, PushPull, PC13};
-use stm32f1xx_hal::prelude::*;
+use stm32f1xx_hal::{
+    gpio::{
+        gpiob::{PB6, PB7},
+        Alternate, OpenDrain,
+    },
+    i2c::{BlockingI2c, DutyCycle, Mode},
+    pac,
+    prelude::*,
+    rcc::Clocks,
+};
 
-systick_monotonic!(Mono, 1000);
+#[entry]
+fn main() -> ! {
+    rtt_init_print!();
+    rprintln!("RTT initialized!");
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let dp = pac::Peripherals::take().unwrap();
+    // dp.I2C1.cr1.modify(|_, w| w.pe().clear_bit()); // Disable I2C
+    // dp.I2C1.cr1.modify(|_, w| w.pe().set_bit()); // Re-enable I2C
+    // rprintln!("I2C reset!");
 
-#[app(device = stm32f1xx_hal::pac, peripherals = true, dispatchers = [SPI1])]
-mod app {
-    use super::*;
+    let mut flash = dp.FLASH.constrain();
+    let mut rcc = dp.RCC.constrain();
+    let clocks: Clocks = rcc.cfgr.freeze(&mut flash.acr);
 
-    #[shared]
-    struct Shared {}
+    let mut delay = Delay::new(cp.SYST, clocks.sysclk().to_Hz());
 
-    #[local]
-    struct Local {
-        led: PC13<Output<PushPull>>,
-        state: bool,
-    }
+    let mut afio = dp.AFIO.constrain();
+    let mut gpiob = dp.GPIOB.split();
+    afio.mapr.modify_mapr(|_, w| w.i2c1_remap().set_bit());
 
-    #[init]
-    fn init(cx: init::Context) -> (Shared, Local) {
-        // Setup clocks
-        let mut flash = cx.device.FLASH.constrain();
-        let rcc = cx.device.RCC.constrain();
+    let scl = gpiob.pb6.into_alternate_open_drain(&mut gpiob.crl);
+    let sda = gpiob.pb7.into_alternate_open_drain(&mut gpiob.crl);
 
-        // Initialize the systick interrupt & obtain the token to prove that we did
-        Mono::start(cx.core.SYST, 36_000_000);
+    let mut i2c = BlockingI2c::i2c1(
+        dp.I2C1,        // I2C1 peripheral
+        (scl, sda),     // SDA & SCL pins
+        &mut afio.mapr, // Alternate function remap
+        Mode::Standard {
+            frequency: 100.kHz(),
+        },
+        clocks, // Clocks
+        1000,   // Start timeout (us)
+        10,     // Start retries
+        5000,   // Address timeout (us)
+        5000,   // Data timeout (us)
+    );
+    // rprintln!("Scanning I2C bus...");
+    // for addr in 0x08..=0x77 {
+    //     if i2c.write(addr, &[0]).is_ok() {
+    //         rprintln!("Device found at 0x{:X}", addr);
+    //     }
+    // }
+    rprintln!("Scan complete.");
 
-        rtt_init_print!();
-        rprintln!("init");
+    let mut mpu = Mpu6050::new(i2c);
+    mpu.init(&mut delay).unwrap(); // Pass delay reference
 
-        let _clocks = rcc
-            .cfgr
-            .use_hse(8.MHz())
-            .sysclk(36.MHz())
-            .pclk1(36.MHz())
-            .freeze(&mut flash.acr);
+    loop {
+        let accel = mpu.get_acc().unwrap();
+        let gyro = mpu.get_gyro().unwrap();
 
-        // Setup LED
-        let mut gpioc = cx.device.GPIOC.split();
-        let led = gpioc
-            .pc13
-            .into_push_pull_output_with_state(&mut gpioc.crh, PinState::Low);
+        let ax = accel.x;
+        let ay = accel.y;
+        let az = accel.z;
+        let gx = gyro.x;
+        let gy = gyro.y;
+        let gz = gyro.z;
 
-        // Schedule the blinking task
-        blink::spawn().ok();
+        rprintln!("AX: {}, AY: {}, AZ: {}", ax, ay, az);
+        rprintln!("GX: {}, GY: {}, GZ: {}", gx, gy, gz);
 
-        (Shared {}, Local { led, state: false })
-    }
-
-    #[task(local = [led, state])]
-    async fn blink(cx: blink::Context) {
-        loop {
-            rprintln!("blink");
-            if *cx.local.state {
-                cx.local.led.set_high();
-                *cx.local.state = false;
-            } else {
-                cx.local.led.set_low();
-                *cx.local.state = true;
-            }
-            Mono::delay(1000.millis()).await;
-        }
+        delay.delay_ms(100u32);
     }
 }
