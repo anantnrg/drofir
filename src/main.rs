50cb68ed6ba0c947503e4a4e33f194f02bb752a5
Anant Narayan <anantnrg@gmail.com>
2025-03-21 07:49:51 +0530

    works. Friendship ended with TIM2, TIM3 is now my friend

--- src/main.rs
+++ src/main.rs
@@ -3,117 +3,49 @@
 
 use cortex_m::delay::Delay;
 use cortex_m_rt::entry;
-use embedded_hal::i2c::I2c;
-use mpu6050::Mpu6050;
+
 use panic_halt as _;
 use rtt_target::{rprintln, rtt_init_print};
 use stm32f1xx_hal::{
-    i2c::{BlockingI2c, Mode},
     pac,
     prelude::*,
-    rcc::Clocks,
-    timer::{Channel, Tim2NoRemap},
+    timer::{pwm, Tim3NoRemap},
 };
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
-    rprintln!("RTT initialized!");
 
-    let cp = cortex_m::Peripherals::take().unwrap();
     let dp = pac::Peripherals::take().unwrap();
+    let cp = cortex_m::Peripherals::take().unwrap();
 
-    let mut flash = dp.FLASH.constrain();
-    let mut rcc = dp.RCC.constrain();
-    let clocks: Clocks = rcc.cfgr.freeze(&mut flash.acr);
+    let rcc = dp.RCC.constrain();
+    let clocks = rcc.cfgr.freeze(&mut dp.FLASH.constrain().acr);
 
     let mut delay = Delay::new(cp.SYST, clocks.sysclk().to_Hz());
 
     let mut afio = dp.AFIO.constrain();
     let mut gpioa = dp.GPIOA.split();
-    let mut gpiob = dp.GPIOB.split();
-    afio.mapr.modify_mapr(|_, w| w.i2c1_remap().set_bit());
+    let servo_pin = gpioa.pa6.into_alternate_push_pull(&mut gpioa.crl);
 
-    let scl = gpiob.pb6.into_alternate_open_drain(&mut gpiob.crl);
-    let sda = gpiob.pb7.into_alternate_open_drain(&mut gpiob.crl);
-    let servo_1 = gpioa.pa0.into_alternate_push_pull(&mut gpioa.crl);
-    let pins = (servo_1);
+    let pwm = dp
+        .TIM3
+        .pwm_hz::<Tim3NoRemap, _, _>(servo_pin, &mut afio.mapr, 50.Hz(), &clocks);
 
-    let mut pwm = dp
-        .TIM2
-        .pwm_hz::<Tim2NoRemap, _, _>(pins, &mut afio.mapr, 50.Hz(), &clocks);
+    let mut channel = pwm.split();
+    channel.enable();
 
-    let max_duty = pwm.get_max_duty(); // Timer resolution
-    let min_pulse = max_duty / 30; // 500us
-    let max_pulse = max_duty / 7; // 2500us
-    pwm.set_duty(Channel::C1, max_pulse);
-    pwm.enable(Channel::C1);
-    rprintln!("Duty cycle for 0°: {}", min_pulse);
-    rprintln!("Max Duty: {}", max_duty);
-    rprintln!(
-        "500us in Ticks: {} | 2500us in Ticks: {}",
-        pulse_width_to_ticks(500, max_duty.into()),
-        pulse_width_to_ticks(2500, max_duty.into())
-    );
-    let max_duty_ticks = pulse_width_to_ticks(max_pulse, max_duty.into());
-    let min_duty_ticks = pulse_width_to_ticks(min_pulse, max_duty.into());
-    rprintln!(
-        "Initial Min Duty Ticks: {} | Initial Max Duty Ticks: {}",
-        min_duty_ticks,
-        max_duty_ticks
-    );
+    let max_duty = channel.get_max_duty();
+    let min_pulse = max_duty / 40;
+    let max_pulse = max_duty / 8;
 
-    let mut i2c = BlockingI2c::i2c1(
-        dp.I2C1,
-        (scl, sda),
-        &mut afio.mapr,
-        Mode::Standard {
-            frequency: 100.kHz(),
-        },
-        clocks,
-        1000,
-        10,
-        5000,
-        5000,
-    );
-
-    let mut mpu = Mpu6050::new(i2c);
-    mpu.init(&mut delay).unwrap();
     loop {
-        pwm.set_duty(Channel::C1, angle_to_duty(0));
+        rprintln!("Moving to 0 degrees with pulse: {}", min_pulse);
+        channel.set_duty(min_pulse);
         delay.delay_ms(1000);
 
-        pwm.set_duty(Channel::C1, angle_to_duty(90));
-
-        delay.delay_ms(1000);
-
-        pwm.set_duty(Channel::C1, angle_to_duty(180));
-
+        rprintln!("Moving to 180 degrees with pulse: {}", max_pulse);
+        channel.set_duty(max_pulse);
         delay.delay_ms(1000);
     }
 }
-
-fn angle_to_duty(angle: u16) -> u16 {
-    let min_pulse = 500; // 500us
-    let max_pulse = 2500; // 2500us
-    min_pulse + (((max_pulse - min_pulse) as u32 * angle as u32) / 180) as u16
-}
-
-fn pulse_width_to_ticks(pulse_width: u16, timer_freq: u32) -> u16 {
-    let pwm_period = 20_000; // 20ms (50Hz)
-    ((pulse_width as u32 * timer_freq) / (1_000_000 / pwm_period)) as u16
-}
-
-// loop {
-//     let accel = mpu.get_acc().unwrap();
-//     let gyro = mpu.get_gyro().unwrap();
-
-//     rprintln!(
-//         "AX: {:.2}, AY: {:.2}, AZ: {:.2} | GX: {:.2}, GY: {:.2}, GZ: {:.2}",
-//         accel.x,
-//         accel.y,
-//         accel.z,
-//         gyro.x,
-//         gyro.y,
-//         gyro.z
-//     );
